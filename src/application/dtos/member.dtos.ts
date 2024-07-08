ab1222820cabbca97ce85f0c5088fb758d61cab2
nottocoe <nottocoe@gmail.com>
2024-07-08 16:17:30 +0700

    update swagger example

--- src/application/dtos/member.dtos.ts
+++ src/application/dtos/member.dtos.ts
@@ -1,21 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class GetMemberResponseDto{
-    @ApiProperty({ example: 'John Doe', description: 'The name of the member' })
+    @ApiProperty({ example: '1234', description: 'Member Id' })
     readonly id: string;
 
-    @ApiProperty({ example: 'john.doe@example.com', description: 'The email address of the member' })
+    @ApiProperty({ example: 'John Doe', description: 'The name of the member' })
     readonly name: string;
 
-    @ApiProperty({ example: 'trisattawat.mekchay@kingpower.com', description: 'The email address of the member' })
+    @ApiProperty({ example: 'john.doe@mail.com', description: 'The email address of the member' })
     readonly email: string;
 }
 
 export class CreateMemberResponseDto {
-    @ApiProperty({ example: 'Trisattawat Mekchay', description: 'The name of the member' })
+    @ApiProperty({ example: 'John Doe', description: 'The name of the member' })
     readonly name: string;
 
-    @ApiProperty({ example: 'trisattawat.mekchay@kingpower.com', description: 'The email address of the member' })
+    @ApiProperty({ example: 'john.doe@mail.com', description: 'The email address of the member' })
     readonly email: string;
 
     @ApiProperty({ example: 30, description: 'The age of the member' })
