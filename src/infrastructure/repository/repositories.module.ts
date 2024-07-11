14c89970b503ce3a2055d6320cf36608d97176e2
Trisattawat <trisattawat.mekchay@kingpower.com>
2024-07-11 15:00:31 +0700

    update interface key to register

--- src/infrastructure/repository/repositories.module.ts
+++ src/infrastructure/repository/repositories.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/infrastructure/persistence/prisma/prisma.service';
 import { MemberRepository } from './member/member.repository';
+import { MEMBER_REPOSITORY } from 'src/common/shared/common';
 
 @Module({
   providers: [
     PrismaService,
     {
-      provide: 'IMemberRepository',
+      provide: MEMBER_REPOSITORY,
       useClass: MemberRepository,
     },
   ],
-  exports: ['IMemberRepository'],
+  exports: [MEMBER_REPOSITORY],
 })
 export class RepositoriesModule {}
