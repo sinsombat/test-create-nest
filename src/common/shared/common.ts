43e44d035d31f56aa7a041262cf52c1ee80d3d6d
Trisattawat <trisattawat.mekchay@kingpower.com>
2024-07-11 15:00:51 +0700

    add core interface key to register in module

---
+++ src/common/shared/common.ts
@@ -0,0 +1,5 @@
+// services key interface for register in modules
+export const MEMBER_SERVICE = 'IMemberServices';
+
+// repository key interface for register in module
+export const MEMBER_REPOSITORY = 'IMemberRepository';
