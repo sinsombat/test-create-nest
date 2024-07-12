7c3dba40aae1af5f46c38dc07e292e0dd31fdce8
Trisattawat <trisattawat.mekchay@kingpower.com>
2024-07-12 11:34:47 +0700

    remove

--- src/infrastructure/persistence/prisma/migrations/20240710094223_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `age` on table `Member` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Member" ALTER COLUMN "age" SET NOT NULL;
